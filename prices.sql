dc6510ff17ad3ffdba103df2ac94eee6a9342d5d
Andrii <andriiplytka@gmail.com>
2024-04-03 21:29:22 +0300

    add price sql

---
+++ prices.sql
@@ -0,0 +1,2 @@
+INSERT INTO webapp.prices (from_supplire, for_client, created_at, deleted_at, good_id) VALUES (25, 30.2, '2024-04-03', null, 1);
+INSERT INTO webapp.prices (from_supplire, for_client, created_at, deleted_at, good_id) VALUES (40, 65.34, '2024-04-03', null, 2);
